532de20cbb54195e989bb047a4700553ac55e8d9
bliu14 <143124039+bliu14@users.noreply.github.com>
2024-04-15 22:17:53 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,12 +7,21 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	    String s = Integer.toString(num);
+	    int[] ans = new int[s.length()];
+	    for(int i = ans.length-1 ; i >=0; i--) {
+	    	ans[i] = num%10;
+		num/=10;
+	    }
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+		for(int i = 0; i < ans.length-1; i++) {
+			if(ans[i] >= ans[i]) {
+				return false;
+		}
+		return true;
 
 	}
 	
